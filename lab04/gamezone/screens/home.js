2c9d076f33c75989d05f74347f4110d8fae3a0ef
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-09-27 12:18:26 -0400

    changes to home.js

--- lab04/gamezone/screens/home.js
+++ lab04/gamezone/screens/home.js
@@ -3,7 +3,11 @@ import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native
 import { globalStyles } from '../styles/global';
 
 export default function Home({ navigation }) {
-    const [reviews, setReviews] = useState([]);
+    const [reviews, setReviews] = useState([
+        { title: 'Sponge Bob', rating: 5, body:'lorem ipsum', key: '1'},
+        { title: 'Batman', rating: 2, body: 'lorem ipsum', key: '2'},
+        { title: 'Up', rating: 4, body: 'lorem ipsum', key: '3'}
+    ]);
 
     return (
         <View style={globalStyles.container}>
