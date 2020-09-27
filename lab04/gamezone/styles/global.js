f75f49057e9d0877c89db8a27d9f0149a00cb566
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-09-27 12:09:52 -0400

    changes to global.js

--- lab04/gamezone/styles/global.js
+++ lab04/gamezone/styles/global.js
@@ -2,11 +2,13 @@ import { StyleSheet } from 'react-native';
 
 export const globalStyles = StyleSheet.create({
   titleText: {
+    fontFamily: 'nunito-bold',
     fontSize: 18,
     fontWeight: 'bold',
     color: '#333',
   },
   paragraph: {
+    fontFamily: 'nunito-regular',
     marginVertical: 8,
     lineHeight: 20,
   },
