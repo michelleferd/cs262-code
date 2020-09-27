5753c4e1ec421a8a7fe070c4472de2b0effba6d7
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-09-26 20:50:43 -0400

    changes to App.js

--- homework1/my-project/App.js
+++ homework1/my-project/App.js
@@ -5,19 +5,63 @@ import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+  {text: 'fireworks', key: '1'},
+  {text: 'pirates', key: '2'},
+  {text: 'ship', key: '3'},
+  ]);
+    const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    });
+  };
+    const submitHandler = (text) => {
+    if (text.length > 0) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+                    ...prevTodos
+        ];
+      });
+    } else {
+      Alert.alert('Oops!', 'Todos must be at least 1 character long.', [
+        { text: 'Understood', onPress: () => console.log('alert closed') }
+      ]);
+    }
 
+  }
   return (
-    <TouchableWithoutFeedback>
-      <View>
+          <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard')
+    }}>
+      <View style={styles.container}>
         <Header />
-        <View>
-          <AddTodo />
-          <View>
-            <FlatList />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
           </View>
         </View>
+
       </View>
     </TouchableWithoutFeedback>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20,
+  },
+});
