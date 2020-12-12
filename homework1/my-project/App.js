fd0154f0c2919703b9eb6cd2b1800136553ec86a
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-12-12 18:58:03 -0500

    homework1

--- homework1/my-project/App.js
+++ homework1/my-project/App.js
@@ -5,63 +5,66 @@ import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
 
 export default function App() {
-  const [todos, setTodos] = useState([
-  {text: 'fireworks', key: '1'},
-  {text: 'pirates', key: '2'},
-  {text: 'ship', key: '3'},
-  ]);
-    const pressHandler = (key) => {
+  const [todos, setTodos] = useState([]);
+
+  const pressHandler = (key) => {
     setTodos((prevTodos) => {
       return prevTodos.filter(todo => todo.key != key)
     });
-  };
-    const submitHandler = (text) => {
-    if (text.length > 0) {
+  }
+
+  const submitHandler = (text) => {
+
+    if(text.length > 3){
       setTodos((prevTodos) => {
         return [
           { text: text, key: Math.random().toString() },
-                    ...prevTodos
+          ...prevTodos
         ];
       });
-    } else {
-      Alert.alert('Oops!', 'Todos must be at least 1 character long.', [
-        { text: 'Understood', onPress: () => console.log('alert closed') }
+    }else{
+      Alert.alert("Well darn,", "Todos must be over 3 chars long", [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
       ]);
     }
 
+    
   }
+
   return (
-          <TouchableWithoutFeedback onPress={() => {
-      Keyboard.dismiss();
-      console.log('dismissed keyboard')
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss(); 
+      console.log('dismissed keyboard');
     }}>
       <View style={styles.container}>
         <Header />
         <View style={styles.content}>
-          <AddTodo submitHandler={submitHandler} />
+          <AddTodo submitHandler={submitHandler}/>
           <View style={styles.list}>
-            <FlatList
+            <FlatList 
               data={todos}
-              renderItem={({ item }) => (
-                <TodoItem item={item} pressHandler={pressHandler} />
+              renderItem={({item}) => (
+                <TodoItem item={item} pressHandler={pressHandler}/>
               )}
             />
           </View>
         </View>
-
       </View>
     </TouchableWithoutFeedback>
   );
 }
-const styles = StyleSheet.create({
+
+const styles =  StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
   content: {
-    padding: 40
+    flex: 1,
+    padding: 40,
   },
   list: {
     marginTop: 20,
-  },
-});
+    flex: 1,
+  }
+})
